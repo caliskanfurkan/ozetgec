1831940ba3b9e8f8f20bd82e6cc0238cb65928cb
Furkan <caliskanfurkan@gmail.com>
2013-04-08 23:10:50 +0300

    creating commenting scripts

---
+++ crawler.py
@@ -0,0 +1,1 @@
+# This script will start to crawl & index Turkish news sites one day
---
+++ sumnews.py
@@ -0,0 +1,2 @@
+# This script will do summmarizing news with a batch manner 
+# and refresh them as summed content into the db
